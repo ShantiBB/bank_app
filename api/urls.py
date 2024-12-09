89c4af36a266f0eb5dbc8f017522b59200011047
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-12-09 03:50:08 +0300

    add api urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api.views import AccountViewSet
+from api.views import TransactionViewSet
+
+router = DefaultRouter()
+router.register(
+    r'accounts',
+    AccountViewSet,
+    basename='accounts'
+)
+router.register(
+    r'transactions',
+    TransactionViewSet,
+    basename='transactions'
+)
+
 
 urlpatterns = [
     path('', include('djoser.urls')),
-    path(r'', include('djoser.urls.authtoken')),
-]
+    path('', include('djoser.urls.authtoken')),
+] + router.urls
